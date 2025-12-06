dabb733d93633a2c327e962c71faec8100438f3a
achrefjalloul <abdeljalilachref@gmail.com>
2025-12-06 23:45:24 +0100

    feat: add real dashboard titles

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { BaseComponent } from 'src/app/modules/shared/components/base-component/
 interface Card {
     id: number;
     title: string;
+    titleEn: string;
     description: string;
     icon: string;
 }
@@ -16,38 +17,45 @@ export class DashboardComponent extends BaseComponent {
     cards: Card[] = [
         {
             id: 1,
-            title: 'المسح الميداني',
-            description:
-                'هنا يمكنك تخصيص وصف في أربعة أسطر هنا يمكنك تخصيص وصف في أربعة أسطر.',
-            icon: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z',
+            title: 'التحكم والتوجيه',
+            titleEn: 'command and control',
+            description: 'command_and_control_desc',
+            icon: 'assets/icons/command_control.svg',
         },
         {
             id: 2,
-            title: 'التحكم والتوجيه',
-            description:
-                'هنا يمكنك تخصيص وصف في أربعة أسطر هنا يمكنك تخصيص وصف في أربعة أسطر.',
-            icon: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z',
+            title: 'المسح الميداني',
+            titleEn: 'Field Survey',
+            description: 'field_survey_desc',
+            icon: 'assets/icons/field_survey.svg',
         },
         {
             id: 3,
-            title: 'الإسناد الآلي',
-            description:
-                'هنا يمكنك تخصيص وصف في أربعة أسطر هنا يمكنك تخصيص وصف في أربعة أسطر.',
-            icon: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z',
+            title: 'الإسناد الأمني',
+            titleEn: 'Security Support',
+            description: 'security_support_desc',
+            icon: 'assets/icons/security_support.svg',
         },
         {
             id: 4,
             title: 'الحماية الميدانية',
-            description:
-                'هنا يمكنك تخصيص وصف في أربعة أسطر هنا يمكنك تخصيص وصف في أربعة أسطر.',
-            icon: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z',
+            titleEn: 'Field Protection',
+            description: 'field_protection_desc',
+            icon: 'assets/icons/field_protection.svg',
         },
         {
             id: 5,
-            title: 'التجهيزات القتالية',
-            description:
-                'هنا يمكنك تخصيص وصف في أربعة أسطر هنا يمكنك تخصيص وصف في أربعة أسطر.',
-            icon: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z',
+            title: 'التجهيزات الأمنية',
+            titleEn: 'Security Equipment',
+            description: 'security_equipment_desc',
+            icon: 'assets/icons/security_equipment.svg',
+        },
+        {
+            id: 6,
+            title: 'الإعدادات العامة',
+            titleEn: 'General Settings',
+            description: 'general_settings_desc',
+            icon: 'assets/icons/general_settings.svg',
         },
     ];
 
